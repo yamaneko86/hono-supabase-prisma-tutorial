2728c603e57ccc60f99a7f64268489091728ed81
yamaneko86 <yamaneko8699@gmail.com>
2024-03-31 08:05:33 +0900

    DB migration

---
+++ prisma/migrations/20240330141850_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "user_id" TEXT NOT NULL,
+    "user_name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("user_id")
+);
---
+++ prisma/migrations/20240330143244_user_id_modified/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "user_id" SET DEFAULT uuid_generate_v4();
