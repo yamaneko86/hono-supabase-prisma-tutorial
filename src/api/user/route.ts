7a9d4f60b405990877fababb995185440f391de3
yamaneko86 <yamaneko8699@gmail.com>
2024-03-31 09:36:17 +0900

    prismaインスタンス生成をメソッド外に移動

--- src/api/user/route.ts
+++ src/api/user/route.ts
@@ -2,9 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 
 const app = new Hono();
+const prisma = new PrismaClient();
 
 export const users = app.get("/", async (c) => {
-  const prisma = new PrismaClient();
   const getUsers = await prisma.user.findMany();
   return c.json(getUsers);
 });
