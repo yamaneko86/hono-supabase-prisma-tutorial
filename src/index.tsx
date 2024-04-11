1b681e6318442951527a1dc4055cd0dffb46033d
yamaneko86 <yamaneko8699@gmail.com>
2024-04-11 20:03:53 +0900

    CDNによるTailwindCSSの導入

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,12 @@ const app = new Hono();
 const Layout: FC = (props) => {
   return (
     <html lang="ja">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Document</title>
+        <script src="https://cdn.tailwindcss.com" />
+      </head>
       <body>{props.children}</body>
     </html>
   );
@@ -16,17 +22,37 @@ const Layout: FC = (props) => {
 const App: FC = () => {
   return (
     <Layout>
-      <h1>ユーザー登録</h1>
-      <form action="/create" method="post">
-        <div>
-          <label htmlFor="username">ユーザーネーム</label>
-          <input name="username" placeholder="Input your name!!" required />
-        </div>
-        <div>
-          <button type="submit">登録</button>
-        </div>
-      </form>
-      <a href="/users">ユーザーリスト</a>
+      <div class="max-w-md mx-auto bg-white p-8 rounded-md shadow-md">
+        <h1 class="text-2xl font-semibold mb-4">ユーザー登録</h1>
+        <form action="/create" method="post">
+          <div class="mb-4">
+            <label
+              for="username"
+              class="block text-sm font-medium text-gray-700"
+            >
+              ユーザーネーム
+            </label>
+            <input
+              name="username"
+              type="text"
+              placeholder="Input your name!!"
+              required
+              class="mt-1 p-2 block w-full border border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              class="py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              登録
+            </button>
+          </div>
+        </form>
+        <a href="/users" class="block mt-4 text-blue-500 hover:underline">
+          ユーザーリスト
+        </a>
+      </div>
     </Layout>
   );
 };
