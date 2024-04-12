a8f272bb7dae036fdc99b2de6769c3a91d823d03
yamaneko86 <yamaneko8699@gmail.com>
2024-04-12 18:14:51 +0900

    タイトル変更

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const Layout: FC = (props) => {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Document</title>
+        <title>Hono × Supabase × Prisma</title>
         <script src="https://cdn.tailwindcss.com" />
       </head>
       <body>{props.children}</body>
